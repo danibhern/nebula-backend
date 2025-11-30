c080ea92447c685cffce8b58de288fec4a61f461
yesi <yes.jara@duocuc.cl>
2025-11-29 23:00:59 -0300

    contact

---
+++ src/main/java/org/nebulabackend/Model/Contact.java
@@ -0,0 +1,36 @@
+package org.nebulabackend.Model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "contacto")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(nullable = false)
+    private String asunto;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

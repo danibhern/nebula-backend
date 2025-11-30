cba864d6065f6e6e507d6bb26be55bef23a53fff
yesi <yes.jara@duocuc.cl>
2025-11-29 23:37:34 -0300

    listo cintac

--- src/main/java/org/nebulabackend/Model/Contact.java
+++ src/main/java/org/nebulabackend/Model/Contact.java
@@ -33,4 +33,44 @@ public class Contact {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
